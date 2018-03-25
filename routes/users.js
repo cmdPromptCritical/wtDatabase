160506df19946d3983c46fe7ccf1424d9a343ff8
cmdPromptCritical <test@gmail.com>
2018-03-24 21:04:53 -0400

    Added simple bootstrap header

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:derp', function(req, res, next) {
-  res.render('index', { title: req.params.derp });
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
