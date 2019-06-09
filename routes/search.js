e8f093b54556f0042d505dc9b5733bf3c53326b8
cmdPromptCritical <7504765+cmdPromptCritical@users.noreply.github.com>
2019-06-08 21:48:34 -0400

    basic pagination GUI framework

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../db');
 let queryRes;
 
-function queryTxt(query, res) {
+function queryTxt(query, res, pgNum = 0) {
   let queryStr = "select generate_series(1,10000000) AS n, cat1, cat2, cat3, cat4, pg, ts_headline(bodytxt, plainto_tsquery('" + query + "'), 'MaxFragments=3, MaxWords=45') from wt_docs WHERE searchtext @@ plainto_tsquery('" + query + "') LIMIT 20;"
   let searchdb = new Promise((resolve, reject) => {
     db.query(queryStr, (err, res) => {
@@ -15,13 +15,13 @@ function queryTxt(query, res) {
       } else {
           queryRes = res.rows
   
-          console.log(queryRes)
+          //console.log(queryRes)
         resolve()
       }
     });
   })
   searchdb.then(() => {
-    res.render('search', { title: 'Search Results for "' + query + '"' , searchResult: queryRes});
+    res.render('search', { title: 'Search Results for "' + query + '"' , searchResult: queryRes, pgNum: pgNum});
   
   }).catch( () => {
     res.render('search', { title: 'Search Results for "' + query + '"'});
@@ -30,15 +30,21 @@ function queryTxt(query, res) {
 
 /* GET search page. */
 router.get('/', (req, res, next) => {
-  console.log(req.query.q)
-  let searchdb = queryTxt(req.query.q, res)
+  let queryTxt = req.query.q
+  let pgNum = req.query.p
 
-});
-
-/* POST the content from the form and do something with it. */
-router.post('/', (req, res) => {
-  queryTxt(req.body.searchString, res)
+  console.log(queryTxt)
+  console.log(pgNum)
   
+  let searchdb = queryTxt(queryTxt, res, pgNum)
+
 });
+
+// commented out bc I don't think I'm using POST anywerhe
+/* POST the content from the form and do something with it. */
+//router.post('/', (req, res) => {
+//  queryTxt(req.body.searchString, res)
+  
+//});
 
 module.exports = router;
