b3d15471e7fcf9ba1476512d34599f12b2b5f5fa
cmdPromptCritical <7504765+cmdPromptCritical@users.noreply.github.com>
2019-08-26 16:41:48 -0400

    added: separate browse nav template, click listener

--- routes/browse.js
+++ routes/browse.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+// grabs all /browse GET requests
+// /browse sends the top level of the 
 /* GET about page. */
-router.get('/', function(req, res, next) {
-  res.render('browse', { title: 'Browse' });
+router.get('/:cat1?/:cat2?/:cat3?/:cat4?', function(req, res, next) {
+  console.log(req.params);
+  if (req.params.cat1) {
+    res.render('browseDrilldown', {layout: 'browseDrilldown', title: 'Browsing :D'});
+  } else {
+    res.render('browse', { title: 'Browse' });
+  }
+  
+
 });
 
 module.exports = router;
