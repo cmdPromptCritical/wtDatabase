fd6a0af61f15d87033c06b6f022332691dc1e8bb
cmdpromptcritical <whatever@gmail.com>
2022-03-29 00:24:55 -0400

    code cleanup

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Watchtower Archive' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Watchtower Archives' });
 });
 
 module.exports = router;
