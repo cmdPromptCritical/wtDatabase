367673d3a1fc663b51ebdedaa4a95cca2cbb1695
adam <adam@live.com>
2022-11-27 10:18:37 +0100

    updated Dockerfile for pm2, added proxy trust to app.js

--- app.js
+++ app.js
@@ -58,6 +58,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// tells server that it is behind a proxy
+app.enable('trust proxy')
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
